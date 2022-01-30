316c48ae2433a12060fa3546e6e4f4b0cb1facf1
hasanalit <tuxtayevhasan72@gmail.com>
2022-01-30 15:30:30 +0500

    avto movie

---
+++ js/amazon.js
@@ -0,0 +1,224 @@
+let amazon = [
+  {
+    "title": "Радар-детектор Inspector Spirit Air",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "1",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 125.8,
+    "garantiya": "Гарантийный срок (месяц): 12",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Радар-детектор Inspector Spirit Air с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Радар-детектор Inspector Spirit Air на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/HRccowyQutz18Kl0BqIkzEOnbuR290J3PXWBxZrLGdhbwlkW3PhAD16hFEKv.jpeg",
+  },
+  {
+    "title": "Радар-детектор Magma R5",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "2",
+    "imdbRating": "⭐️⭐️⭐️⭐️",
+    "price": 125.8,
+    "garantiya": "Гарантийный срок (месяц): 1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Радар-детектор Magma R5 с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Радар-детектор Magma R5 на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/59SGFbBorJoENWwQgLYJSbbpedPRL4tafQhQtAxEoEf8KAkNZ7R3uCpZb1xc.jpeg",
+  },
+  {
+    "title": "Видеорегистратор iFound Q66S",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "3",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 90.8,
+    "garantiya": "Гарантийный срок (месяц): 1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Видеорегистратор iFound Q66S с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Видеорегистратор iFound Q66S на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/TgR8dlUIWVhC3Ofg6lcGdHEaQph3n6NRYuqaENprsJaR6WLQCQrfQ2CDKjAp.jpeg",
+  },
+  {
+    "title": "Видеорегистратор iFound V88",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "4",
+    "imdbRating": "⭐️⭐️⭐️",
+    "price": 85.9,
+    "garantiya": "Гарантийный срок (месяц): 3",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Видеорегистратор iFound V88 с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Видеорегистратор iFound V88 на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/VSslansEXSdhJDF07HgBE10EOWTgUcZ0xLVcYnO3PSVRxvCJdZTOuyCaNmTP.jpeg",
+  },
+  {
+    "title": "Диски для автомобиля NewStyling 005 R17 (4шт)",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "5",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 429.9,
+    "garantiya": "Гарантийный срок (месяц): 1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Диски для автомобиля NewStyling 005 R17 (4шт) с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Диски для автомобиля NewStyling 005 R17 (4шт) на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/yXPmV9kqHC8lZLTP9Xxy3oPjcfrN0dmNgg5K0VCfvMcJBjHMnmJYYbMDx6LP.jpeg",
+  },
+  {
+    "title": "Диска для автомобиля NewStyle 003 R15 4шт",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "6",
+    "imdbRating": "⭐️⭐️⭐️⭐️",
+    "price": 327.9,
+    "garantiya": "Гарантийный срок (месяц): 2",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Диска для автомобиля NewStyle 003 R15 4шт с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Диска для автомобиля NewStyle 003 R15 4шт на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/aQR8xaNgvM6hqqzNhoQbgDEf872hqPz0ufNyxYOneRxn7kkywoPsJSoIcSsh.jpeg",
+  },
+  {
+    "title": "Диска для автомобиля Chevrolet Redline 15",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "7",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 342.5,
+    "garantiya": "Гарантийный срок (месяц):1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Видеорегистратор iFound V88 с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Видеорегистратор iFound V88 на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/tpUqCeWfQu1AkcScT8HZhBcKAMeFDHFIICuhixkHZsEgf9CLgeshNOzuSDAD.jpeg",
+  },
+  {
+    "title": "Руль Mercedes Tiptronic (анатомия)",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "8",
+    "imdbRating": "⭐️⭐️⭐️⭐️",
+    "price": 107.6,
+    "garantiya": "Гарантийный срок (месяц): 5",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Руль Mercedes Tiptronic (анатомия) с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Руль Mercedes Tiptronic (анатомия) на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/lh4aH0Wl6N2yJgBAyTtxwgy3lX9oMRTSbHwJw8TKaAGZ5vPAkyvlKfLFHEDi.jpeg",
+  },
+  {
+    "title": "Руль Cadillac Tiptronic (анатомия)",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "9",
+    "imdbRating": "⭐️⭐️⭐️",
+    "price": 207.9,
+    "garantiya": "Гарантийный срок (месяц): 1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Руль Cadillac Tiptronic (анатомия) с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Руль Cadillac Tiptronic (анатомия) на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/uUcQIogo3PgvRfiYqh1en3KfhGfOZJA1VjIYmynzbSHE4XXvnJWmRUsf5a02.jpeg",
+  },
+  {
+    "title": "Руль Hyundai Anatomy Tiptronic (анатомия)",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "10",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 240.9,
+    "garantiya": "Гарантийный срок (месяц): 7",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Руль Hyundai Anatomy Tiptronic (анатомия) с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Руль Hyundai Anatomy Tiptronic (анатомия) на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/cHdEVpeKWn1W4ccOLL2daJGm1X9OEOKZ9s9aIvzT52YCdh7wMq6cp4sRGVlB.jpeg",
+  },
+  {
+    "title": "Авточехлы Cardinar Turk-2 Silver Cobalt",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "11",
+    "imdbRating": "⭐️⭐️⭐️",
+    "price": 70.9,
+    "garantiya": "Гарантийный срок (месяц): 2",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Авточехлы Cardinar Turk-2 Silver Cobalt с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Авточехлы Cardinar Turk-2 Silver Cobalt на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/j3SxkCQXpTM9mFSdqtHQqZ05hbhH1jCJ0wTdqp5TA94toQSRlLGWrXM51Ht7.jpeg",
+  },
+  {
+    "title": "Авточехлы Cardinar Turk-1 Silver Lacetti-3",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "12",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 68.9,
+    "garantiya": "Гарантийный срок (месяц): 1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Авточехлы Cardinar Turk-1 Silver Lacetti-3 с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Авточехлы Cardinar Turk-1 Silver Lacetti-3 на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/xoxJaqJBtYpIvLoyf5PwrRvDmjS35DNmGk6mpbHDu2NrAZoZ7D4Eb3g7OQrW.jpeg",
+  },
+  {
+    "title": "Автомагнитола 2 DIN Sony DSX-B700",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "13",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 123.3,
+    "garantiya": "Гарантийный срок (месяц): 2",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Автомагнитола 2 DIN Sony DSX-B700 с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Автомагнитола 2 DIN Sony DSX-B700 на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/zF5weG2kdAEPMSLuUViNHodgJDu3xkuucJNBgU9BtMYsUv8qjlygfvleNr47.jpeg",
+  },
+  {
+    "title": "Автомагнитола Sony CDX-G1200U",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "14",
+    "imdbRating": "⭐️⭐️⭐️",
+    "price": 113.2,
+    "garantiya": "Гарантийный срок (месяц): 1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Автомагнитола Sony CDX-G1200U с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Автомагнитола Sony CDX-G1200U на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/8ar4x4xzShdFNUCL9TLtNrfIErP7M0AmPbyEkdPJdPNcOceBB0v3z2s6ql1r.jpeg",
+  },
+  {
+    "title": "Задние фонари Chevrolet Cobalt, красные",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "15",
+    "imdbRating": "⭐️⭐️⭐️⭐️",
+    "price": 300,
+    "garantiya": "Гарантийный срок (месяц): 1",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Задние фонари Chevrolet Cobalt, красные с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Задние фонари Chevrolet Cobalt, красные на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/xgYLTrJAXlYoh3ALurfzeG2yEyK9DR3ew1QYS9AusHop6xuTfD2urjD0VEex.jpeg",
+  },
+  {
+    "title": "Передние фары Lacetti Gentra американка",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "16",
+    "imdbRating": "⭐️⭐️⭐️⭐️⭐️",
+    "price": 380.5,
+    "garantiya": "Гарантийный срок (месяц): 2",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Руль Cadillac Tiptronic (анатомия) с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Руль Cadillac Tiptronic (анатомия) на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/7vR8au4OmHMnLMG3ynRvOSZXFXVAFYfdqYJ4WHSYcqe3QT4zc86kvPs0MfIM.jpeg",
+  },
+  {
+    "title": "Бар-подлокотник Malibu 2",
+    "categories": [
+      "Automtive"
+    ],
+    "id": "17",
+    "imdbRating": "⭐️⭐️⭐️⭐️",
+    "price": 207.9,
+    "garantiya": "Гарантийный срок (месяц): 5",
+    "saralash": "Коротко о товаре",
+    "summary": "Купить Руль Cadillac Tiptronic (анатомия) с доставкой по Ташкенте, Ташкентской области, Андижан, Бухара, Джизак, Кашкадарья, Навоий, Наманган, Самарканд, Сурхандарья, Сырдарья, Фергана, Хорезм, Каракалпакстан. В рассрочку без кредита в рассрочку - цены, отзывы, характеристики, каталог Руль Cadillac Tiptronic (анатомия) на olcha.uz",
+    "smallThumbnail": "https://olcha.uz/image/original/products/ytoRPDBDJEwvY1j03xWe3TFc3shN1etTgdaCulCfcjWHFQGMPZS5c6ixCwyo.jpeg",
+  },
+
+];
