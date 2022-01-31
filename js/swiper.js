e85ef9c645520d489740195882d3a09bc2ab5531
hasanalit <tuxtayevhasan72@gmail.com>
2022-01-31 20:36:42 +0500

    nice

--- js/swiper.js
+++ js/swiper.js
@@ -18,18 +18,29 @@ let elList = document.querySelector('.swiper-list')
 const renderSwip = function(arr, element){
   arr.forEach(amazon => {
       let newSection = document.createElement('div')
+      let newHeroSection = document.createElement('div')
+      let newHeroLeftSection = document.createElement('div')
+      let newdescName = document.createElement('p')
+      let newdescMainName = document.createElement('p')
       let newImg = document.createElement('img')
 
+      newHeroSection.setAttribute('class', 'heading-hero-section')
       newSection.setAttribute('class', 'swiper-slide')
       newImg.setAttribute('src', amazon.smallThumbnail)
+      newdescName.classList.add('class', 'name-desc')
+      newdescMainName.setAttribute('class', 'name-main-desc')
 
+      newImg.style.width = "417px"
 
-      newImg.style.width = '300px'
-
-      newSection.textContent = amazon.title
+      newdescName.textContent = amazon.saralash
+      newdescMainName.textContent = amazon.title
 
       element.appendChild(newSection)
-      newSection.appendChild(newImg)
+      newSection.appendChild(newHeroSection)
+      newHeroSection.appendChild(newHeroLeftSection)
+      newHeroLeftSection.appendChild(newdescName)
+      newHeroLeftSection.appendChild(newdescMainName)
+      newHeroSection.appendChild(newImg)
   })
 }
 
